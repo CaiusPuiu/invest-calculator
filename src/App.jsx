cb4a8fb9dc840ab6c00068d989bbefefc4b75d3a
CaiusPuiu <caiuspuiu@gmail.com>
2024-06-22 20:35:51 +0300

    feat: finished investment calculator functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      {isInputValid ? (
+        <Results userInput={userInput} />
+      ) : (
+        <p className="center">Please enter a DURATION bigger than zero</p>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
